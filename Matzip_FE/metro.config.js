4b217b57a21251a42cfe3851866dd1c47f631c83
강주현 <juhkang@gangjuhyeon-ui-MacBookAir.local>
2025-02-05 18:04:20 +0900

    그외

--- Matzip_FE/metro.config.js
+++ Matzip_FE/metro.config.js
@@ -9,3 +9,21 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// const path = require('path');
+// const { getDefaultConfig } = require('metro-config');
+
+// module.exports = (async () => {
+//   const {
+//     resolver: { sourceExts, assetExts },
+//   } = await getDefaultConfig();
+//   return {
+//     resolver: {
+//       // extraNodeModules를 사용하여 alias를 설정
+//       extraNodeModules: {
+//         '@': path.resolve(__dirname, 'src'),
+//       },
+//       assetExts,
+//       sourceExts,
+//     },
+//   };
+// })();
