4688a6522ee4950bd61b5403e3f24d8125078411
강주현 <juhkang@gangjuhyeon-ui-MacBookAir.local>
2025-02-05 16:51:31 +0900

    eslintrc.js 수정

--- Matzip_FE/.eslintrc.js
+++ Matzip_FE/.eslintrc.js
@@ -1,4 +1,15 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native',
+// };
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@babel/eslint-parser',
+  parseroptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['module:metro-react-native-babel-preset'],
+    },
+  },
 };
