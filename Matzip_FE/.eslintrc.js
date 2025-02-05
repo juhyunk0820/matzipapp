54be5e012bc27c74f83f3f620f895ca9ef556f0c
강주현 <juhyunk0820@naver.com>
2025-02-05 19:41:09 +0900

    설정 변경

--- Matzip_FE/.eslintrc.js
+++ Matzip_FE/.eslintrc.js
@@ -1,16 +1,16 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-};
 // module.exports = {
 //   root: true,
-//   extends: '@react-native-community',
-//   parser: '@babel/eslint-parser',
-//   parseroptions: {
-//     requireConfigFile: false,
-//     babelOptions: {
-//       presets: ['module:metro-react-native-babel-preset'],
-//     },
-//     rules: {},
-//   },
+//   extends: '@react-native',
 // };
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@babel/eslint-parser',
+  parseroptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['module:metro-react-native-babel-preset'],
+    },
+    rules: {},
+  },
+};
