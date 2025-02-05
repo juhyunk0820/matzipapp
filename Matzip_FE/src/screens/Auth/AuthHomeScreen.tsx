a87f27d977c1dca9648795c59c7b40833b476867
강주현 <juhkang@gangjuhyeon-ui-MacBookAir.local>
2025-02-05 19:19:33 +0900

    회원가입 화면 설정- Home

---
+++ Matzip_FE/src/screens/Auth/AuthHomeScreen.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { AuthStackParamList } from "../../navigations/stack/AuthStackNavigator";
+import { StackScreenProps } from "@react-navigation/stack";
+import { authNavigations } from "@/constants/navigation";
+import { StyleSheet, Text, View } from "react-native";
+
+type AuthScreenProps = StackScreenProps<AuthStackParamList, typeof authNavigations.AUTH_HOME>;
+
+const AuthHomeScreen = ({navigation}: AuthScreenProps) => {
+    return(
+        <View>
+           <Text>AuthHomeScreen</Text> 
+        </View>
+    );
+}
+
+export default AuthHomeScreen;
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+});
