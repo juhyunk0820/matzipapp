94ee24fcda871959f1724cc5aac8531878074565
강주현 <juhyunk0820@naver.com>
2025-02-05 22:00:55 +0900

    로그인&회원가입 버튼 생성

--- Matzip_FE/src/screens/Auth/AuthHomeScreen.tsx
+++ Matzip_FE/src/screens/Auth/AuthHomeScreen.tsx
@@ -2,23 +2,66 @@ import React from 'react';
 import {AuthStackParamList} from '@/navigations/stack/AuthStackNavigator';
 import {StackScreenProps} from '@react-navigation/stack';
 import {authNavigations} from '@/constants/navigations';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import CustomButton from '@/components/CustomButton';
 
 type AuthScreenProps = StackScreenProps<
   AuthStackParamList,
   typeof authNavigations.AUTH_HOME
 >;
 
+const deviceWidth = Dimensions.get('screen').width;
+
 const AuthHomeScreen = ({navigation}: AuthScreenProps) => {
   return (
-    <View>
-      <Text>AuthHomeScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require('@/assets/MATZIP.png')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          label="로그인"
+          onPress={() => navigation.navigate(authNavigations.AUTH_LOGIN)}
+        />
+        <CustomButton
+          label="회원가입"
+          variant="outlined"
+          onPress={() => navigation.navigate(authNavigations.AUTH_SIGNUP)}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default AuthHomeScreen;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: deviceWidth / 2,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    flex: 1,
+    padding: 30,
+    gap: 10,
+  },
 });
