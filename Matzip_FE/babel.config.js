283efa8c86629d1faae6ecb8ec2b3dafe654ac7d
강주현 <juhkang@gangjuhyeon-ui-MacBookAir.local>
2025-02-05 18:03:36 +0900

    resolver 추가

--- Matzip_FE/babel.config.js
+++ Matzip_FE/babel.config.js
@@ -1,3 +1,19 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+//   plugins: 
+// };
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['react-native-reanimated/plugin'],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src'
+        },
+      },
+    ],
+  ],
 };
