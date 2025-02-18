9eeb99cc7aa65e3a522c9739abeba21e5932a45a
강주현 <juhyunk0820@naver.com>
2025-02-18 23:37:30 +0900

    GoogleMaps api 사용 관련 설정 변경: Deployment version - 15.6, 위치, 사진 사용관련 허가 문구 설정: info.Plist

--- Matzip_FE/ios/Matzip_FE/AppDelegate.swift
+++ Matzip_FE/ios/Matzip_FE/AppDelegate.swift
@@ -2,10 +2,13 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import GoogleMaps //import 추가
 
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAYYhxz8PvPSi8C25VAa7yOwhHrCAZR0zU")  //googlemaps key 추가
+    
     self.moduleName = "Matzip_FE"
     self.dependencyProvider = RCTAppDependencyProvider()
 
