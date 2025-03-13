6f55bc78688777544c2ba7f5bc10b29907ad467d
강주현 <juhyunk0820@naver.com>
2025-03-13 23:17:20 +0900

    Post 훅

---
+++ Matzip_FE/src/hooks/queries/useMutateCreatePost.ts
@@ -0,0 +1,57 @@
+// import {createPost} from '@/api';
+// import queryClient from '@/api/queryClient';
+// import {queryKeys} from '@/constants';
+// import {UseMutationCustomOptions} from '@/types';
+// import {useMutation} from '@tanstack/react-query';
+
+// function useMuateCreatePost(mutationOptions?: UseMutationCustomOptions) {
+//   return useMutation({
+//     mutationFn: createPost,
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({
+//         queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+//       });
+//     },
+//     ...mutationOptions,
+//   });
+// }
+
+// export default useMuateCreatePost;
+import {useMutation} from '@tanstack/react-query';
+
+import {createPost} from '@/api/post';
+import {Marker, UseMutationCustomOptions} from '@/types';
+import queryClient from '@/api/queryClient';
+import {queryKeys} from '@/constants';
+
+function useMutateCreatePost(mutationOptions?: UseMutationCustomOptions) {
+  return useMutation({
+    mutationFn: createPost,
+    onSuccess: newPost => {
+      // queryClient.invalidateQueries({
+      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      // });
+
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        existingMarkers => {
+          const newMarker = {
+            id: newPost.id,
+            latitude: newPost.latitude,
+            longitude: newPost.longitude,
+            color: newPost.color,
+            score: newPost.score,
+          };
+
+          return existingMarkers
+            ? [...existingMarkers, newMarker]
+            : [newMarker];
+        },
+      );
+    },
+
+    ...mutationOptions,
+  });
+}
+
+export default useMutateCreatePost;
