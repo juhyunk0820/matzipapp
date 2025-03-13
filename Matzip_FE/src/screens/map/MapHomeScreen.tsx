d9edba072d4bf8fdb79e778d99606f9331c1dab8
강주현 <juhyunk0820@naver.com>
2025-03-13 12:19:15 +0900

    커스텀 mapStyle map에 적용

--- Matzip_FE/src/screens/map/MapHomeScreen.tsx
+++ Matzip_FE/src/screens/map/MapHomeScreen.tsx
@@ -1,7 +1,126 @@
+// import React, {useRef} from 'react';
+// import {Pressable, StyleSheet, Text, View} from 'react-native';
+// import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+// import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+// import useAuth from '@/hooks/queries/useAuth';
+// import {colors} from '@/constants';
+// import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
+// import {StackNavigationProp} from '@react-navigation/stack';
+// import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
+// import {DrawerNavigationProp} from '@react-navigation/drawer';
+// import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
+// import useUserLocation from '@/hooks/useUserLocation';
+// import usePermission from '@/hooks/usePermission';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+// import mapStyle from '@/style/mapStyle';
+// // import Icon from '@react-native-vector-icons/fontawesome6';
+// // import Icon from '@react-native-vector-icons/fontawesome6';
+// // import IconCommon from '@react-native-vector-icons/common'
+// // import Icon from 'react-native-vector-icons/FontAwesome6';
+
+// type Navigation = CompositeNavigationProp<
+//   StackNavigationProp<MapStackParamList>,
+//   DrawerNavigationProp<MainDrawerParamList>
+// >;
+
+// function MapHomeScreen() {
+//   const inset = useSafeAreaInsets();
+//   const {logoutMutation} = useAuth();
+//   const navigation = useNavigation<Navigation>();
+//   const mapRef = useRef<MapView | null>(null);
+//   const {userLocation, isUserLocationError} = useUserLocation();
+//   usePermission('LOCATION');
+
+//   const handleLogout = () => {
+//     logoutMutation.mutate(null);
+//   };
+
+//   const handlePressUserLocation = () => {
+//     if (isUserLocationError) {
+//       // 에러메세지를 표시하기
+//       return;
+//     }
+
+//     mapRef.current?.animateToRegion({
+//       latitude: userLocation.latitude,
+//       longitude: userLocation.longitude,
+//       latitudeDelta: 0.0922,
+//       longitudeDelta: 0.0421,
+//     });
+//   };
+
+//   return (
+//     <>
+//       <MapView
+//         ref={mapRef}
+//         style={styles.container}
+//         provider={PROVIDER_GOOGLE}
+//         showsUserLocation
+//         followsUserLocation
+//         showsMyLocationButton={false}
+//         customMapStyle={mapStyle}
+//       />
+//       <Pressable
+//         style={[styles.drawerButton, {top: inset.top || 20}]}
+//         onPress={() => navigation.openDrawer()}>
+//         {/* <Text>서랍</Text> */}
+//         <Ionicons name="menu" size={20} color={colors.WHITE} />
+//       </Pressable>
+//       <View style={styles.buttonList}>
+//         <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+//           {/* <Text>내위치</Text> */}
+//           <Ionicons name="locate" size={20} color={colors.WHITE} />
+//         </Pressable>
+//       </View>
+//     </>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   drawerButton: {
+//     position: 'absolute',
+//     left: 0,
+//     paddingVertical: 10,
+//     paddingHorizontal: 12,
+//     backgroundColor: colors.PINK_700,
+//     borderTopRightRadius: 50,
+//     borderBottomRightRadius: 50,
+//     shadowColor: colors.BLACK,
+//     shadowOffset: {width: 1, height: 1},
+//     shadowOpacity: 0.5,
+//     elevation: 4,
+//   },
+//   buttonList: {
+//     position: 'absolute',
+//     bottom: 30,
+//     right: 15,
+//   },
+//   mapButton: {
+//     backgroundColor: colors.PINK_700,
+//     marginVertical: 5,
+//     height: 48,
+//     width: 48,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     borderRadius: 30,
+//     shadowColor: colors.BLACK,
+//     shadowOffset: {width: 1, height: 2},
+//     shadowOpacity: 0.5,
+//     elevation: 2,
+//   },
+// });
+
+// export default MapHomeScreen;
 import React, {useRef} from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import useAuth from '@/hooks/queries/useAuth';
 import {colors} from '@/constants';
@@ -12,6 +131,7 @@ import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
 import useUserLocation from '@/hooks/useUserLocation';
 import usePermission from '@/hooks/usePermission';
+import mapStyle from '@/style/mapStyle';
 
 type Navigation = CompositeNavigationProp<
   StackNavigationProp<MapStackParamList>,
@@ -53,15 +173,16 @@ function MapHomeScreen() {
         showsUserLocation
         followsUserLocation
         showsMyLocationButton={false}
+        customMapStyle={mapStyle}
       />
       <Pressable
         style={[styles.drawerButton, {top: inset.top || 20}]}
         onPress={() => navigation.openDrawer()}>
-        <Text>서랍</Text>
+        <Ionicons name="menu" color={colors.WHITE} size={25} />
       </Pressable>
       <View style={styles.buttonList}>
         <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
-          <Text>내위치</Text>
+          <MaterialIcons name="my-location" color={colors.WHITE} size={25} />
         </Pressable>
       </View>
     </>
