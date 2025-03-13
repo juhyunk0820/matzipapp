8259173554f0849703f1d723008b876a8f11f7c9
강주현 <juhyunk0820@naver.com>
2025-03-13 23:19:09 +0900

    검증로직 추가: Title

--- Matzip_FE/src/utils/validate.ts
+++ Matzip_FE/src/utils/validate.ts
@@ -1,37 +1,47 @@
 type UserInfomation = {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
+};
+
+function validateUser(values: UserInfomation) {
+  const errors = {
+    email: '',
+    password: '',
   };
-  
-  function validateUser(values: UserInfomation) {
-    const errors = {
-      email: '',
-      password: '',
-    };
-  
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
-      errors.email = '올바른 이메일 형식이 아닙니다.';
-    }
-    if (!(values.password.length >= 8 && values.password.length <= 20)) {
-      errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
-    }
-  
-    return errors;
+
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+    errors.email = '올바른 이메일 형식이 아닙니다.';
   }
-  
-  function validateLogin(values: UserInfomation) {
-    return validateUser(values);
+  if (!(values.password.length >= 8 && values.password.length <= 20)) {
+    errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
   }
-  
-  function validateSignup(values: UserInfomation & {passwordConfirm: string}) {
-    const errors = validateUser(values);
-    const signupErrors = {...errors, passwordConfirm: ''};
-  
-    if (values.password !== values.passwordConfirm) {
-      signupErrors.passwordConfirm = '비밀번호가 일치하지않습니다.';
-    }
-  
-    return signupErrors;
+
+  return errors;
+}
+
+function validateLogin(values: UserInfomation) {
+  return validateUser(values);
+}
+
+function validateSignup(values: UserInfomation & {passwordConfirm: string}) {
+  const errors = validateUser(values);
+  const signupErrors = {...errors, passwordConfirm: ''};
+
+  if (values.password !== values.passwordConfirm) {
+    signupErrors.passwordConfirm = '비밀번호가 일치하지않습니다.';
   }
-  
-  export {validateLogin, validateSignup};
+
+  return signupErrors;
+}
+
+function validateAddPost(values: {title: string; description: string}) {
+  const errors = {title: '', description: ''};
+
+  if (values.title.length < 1) {
+    errors.title = '제목은 1~30자 이내로 입력해주세요.';
+  }
+
+  return errors;
+}
+
+export {validateLogin, validateSignup, validateAddPost};
