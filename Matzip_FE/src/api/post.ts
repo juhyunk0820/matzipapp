2476dcbf3b9fc14546565c0de3c521f16cc7c726
강주현 <juhyunk0820@naver.com>
2025-03-15 23:26:17 +0900

    post 실행로그

--- Matzip_FE/src/api/post.ts
+++ Matzip_FE/src/api/post.ts
@@ -7,7 +7,7 @@ type RequestCreatePost = Omit<Post, 'id'> & {imageUris: ImageUri[]};
 
 const createPost = async (body: RequestCreatePost): Promise<ResponsePost> => {
   const {data} = await axiosInstance.post('/posts', body);
-
+  console.log('data - createPost', data);
   return data;
 };
 
