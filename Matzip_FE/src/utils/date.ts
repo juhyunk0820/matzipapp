5dbb75a7be1036730a7d55b5ee321b146408cacb
강주현 <juhyunk0820@naver.com>
2025-03-20 00:12:28 +0900

    separator 추가 날짜 분리 함수

---
+++ Matzip_FE/src/utils/date.ts
@@ -0,0 +1,18 @@
+function getDateWithSeparator(
+  dateString: Date | string,
+  separator: string = '',
+) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  console.log(year, month, day);
+  return [
+    String(year),
+    String(month).padStart(2, '0'),
+    String(day).padStart(2, '0'),
+  ].join(separator);
+}
+
+export default getDateWithSeparator;
