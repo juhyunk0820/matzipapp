69e7376ee164e0958cf59b2cb58ccb95f35fb155
강주현 <juhyunk0820@naver.com>
2025-03-21 18:25:23 +0900

    useImagePicker hook 수정: 삭제 기능 및 순서 바꾸기 추가

--- Matzip_FE/src/hooks/useImagePicker.ts
+++ Matzip_FE/src/hooks/useImagePicker.ts
@@ -19,6 +19,18 @@ function useImagePicker({initialImages = []}: UseImagePickerProps) {
     setImageUris(prev => [...prev, ...uris.map(uri => ({uri}))]);
   };
 
+  const deleteImageUri = (uri: string) => {
+    const newImageUris = imageUris.filter(image => image.uri !== uri);
+    setImageUris(newImageUris);
+  };
+
+  const changeImageUrisOrder = (fromIndex: number, toIndex: number) => {
+    const copyImageUris = [...imageUris];
+    const [removedImage] = copyImageUris.splice(fromIndex, 1);
+    copyImageUris.splice(toIndex, 0, removedImage);
+    setImageUris(copyImageUris);
+  };
+
   const handleChange = () => {
     ImagePicker.openPicker({
       mediaType: 'photo',
@@ -45,6 +57,8 @@ function useImagePicker({initialImages = []}: UseImagePickerProps) {
   return {
     imageUris,
     handleChange,
+    delete: deleteImageUri,
+    changeOrder: changeImageUrisOrder,
   };
 }
 export default useImagePicker;
