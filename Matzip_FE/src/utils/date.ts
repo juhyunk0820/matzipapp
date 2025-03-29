1920f23abc858d1d40bd23a77f9db5ea8ea00f5c
강주현 <juhyunk0820@naver.com>
2025-03-29 23:54:59 +0900

    datelocale function

--- Matzip_FE/src/utils/date.ts
+++ Matzip_FE/src/utils/date.ts
@@ -1,13 +1,18 @@
-function getDateWithSeparator(
-  dateString: Date | string,
-  separator: string = '',
-) {
+function getDateDetails(dateString: Date | string) {
   const date = new Date(dateString);
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  console.log(year, month, day);
+  return {year, month, day};
+}
+
+function getDateWithSeparator(
+  dateString: Date | string,
+  separator: string = '',
+) {
+  const {year, month, day} = getDateDetails(dateString);
+
   return [
     String(year),
     String(month).padStart(2, '0'),
@@ -15,4 +20,10 @@ function getDateWithSeparator(
   ].join(separator);
 }
 
-export {getDateWithSeparator};
+function getDateLocaleFormat(dateString: Date | string) {
+  const {year, month, day} = getDateDetails(dateString);
+
+  return `${year}년 ${month}월 ${day}일`;
+}
+
+export {getDateWithSeparator, getDateLocaleFormat};
