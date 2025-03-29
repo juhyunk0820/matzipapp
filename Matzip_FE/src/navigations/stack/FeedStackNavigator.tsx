b49dc4df6df3c892a5372adfa7bae327a7d071f4
강주현 <juhyunk0820@naver.com>
2025-03-29 23:56:40 +0900

    Feed 네비게이터 수정: DetailScreen 추가

--- Matzip_FE/src/navigations/stack/FeedStackNavigator.tsx
+++ Matzip_FE/src/navigations/stack/FeedStackNavigator.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {feedNavigations} from '@/constants';
+
+import {colors, feedNavigations} from '@/constants';
 import FeedHomeScreen from '@/screens/feed/FeedHomeScreen';
 import FeedHomeHeaderLeft from '@/components/feed/FeedHomeHeaderLeft';
+import FeedDetailScreen from '@/screens/feed/FeedDetailScreen';
 
 export type FeedStackParamList = {
   [feedNavigations.FEED_HOME]: undefined;
-  //   [feedNavigations.FEED_DETAIL]: undefined;
+  [feedNavigations.FEED_DETAIL]: {id: number};
 };
 
 const Stack = createStackNavigator<FeedStackParamList>();
@@ -35,17 +37,17 @@ function FeedStackNavigator() {
           headerLeft: () => FeedHomeHeaderLeft(navigation),
         })}
       />
-      {/* <Stack.Screen
-        name={feedNavigations.FEED_HOME}
-        component={FeedHomeScreen}
-        options={({navigation}) => ({
-          headerTitle: '피드',
-          headerLeft: () => {
-            const drawerNavigation = navigation.getParent()!; // non-null assertion
-            return FeedHomeHeaderLeft(drawerNavigation);
+      <Stack.Screen
+        name={feedNavigations.FEED_DETAIL}
+        component={FeedDetailScreen}
+        options={{
+          headerShown: false,
+          headerTitle: ' ',
+          cardStyle: {
+            backgroundColor: colors.GRAY_100,
           },
-        })}
-      /> */}
+        }}
+      />
     </Stack.Navigator>
   );
 }
