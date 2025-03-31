2bc5096e464d58082b1e3234811aed698242f067
강주현 <juhyunk0820@naver.com>
2025-03-31 22:53:02 +0900

    FeedDetailOption 수정: 수정 기능 구현 => 수정화면 이동

--- Matzip_FE/src/components/feed/FeedDetailOption.tsx
+++ Matzip_FE/src/components/feed/FeedDetailOption.tsx
@@ -6,7 +6,7 @@ import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {FeedStackParamList} from '@/navigations/stack/FeedStackNavigator';
 import {Alert} from 'react-native';
-import {alerts} from '@/constants';
+import {alerts, feedNavigations} from '@/constants';
 
 interface FeedDetailOptionProps {
   isVisible: boolean;
@@ -42,6 +42,20 @@ function FeedDetailOption({isVisible, hideOption}: FeedDetailOptionProps) {
       },
     ]);
   };
+
+  const handleEditPost = () => {
+    if (!detailPost) {
+      return;
+    }
+    navigation.navigate(feedNavigations.EDIT_POST, {
+      location: {
+        latitude: detailPost.latitude,
+        longitude: detailPost.longitude,
+      },
+    });
+    hideOption();
+  };
+
   return (
     <CompoundOption isVisible={isVisible} hideOption={hideOption}>
       <CompoundOption.Background>
@@ -50,7 +64,9 @@ function FeedDetailOption({isVisible, hideOption}: FeedDetailOptionProps) {
             삭제하기
           </CompoundOption.Button>
           <CompoundOption.Divider />
-          <CompoundOption.Button>수정하기</CompoundOption.Button>
+          <CompoundOption.Button onPress={handleEditPost}>
+            수정하기
+          </CompoundOption.Button>
         </CompoundOption.Container>
         <CompoundOption.Container>
           <CompoundOption.Button onPress={hideOption}>
