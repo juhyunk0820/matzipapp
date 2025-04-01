6b85db79e03c57bdb2aba3f22a54b6feea0b004c
강주현 <juhyunk0820@naver.com>
2025-04-01 18:19:28 +0900

    FeedDetail화면 수정: 즐겨찾기 클릭 시 favorite등록 및 아이콘 색상 변경

--- Matzip_FE/src/screens/feed/FeedDetailScreen.tsx
+++ Matzip_FE/src/screens/feed/FeedDetailScreen.tsx
@@ -34,6 +34,7 @@ import useLocationStore from '@/store/useLocationStore';
 import useModal from '@/hooks/useModal';
 import FeedDetailOption from '@/components/feed/FeedDetailOption';
 import useDetailStore from '@/store/useDetailPostStore';
+import useMutateFavoritePost from '@/hooks/queries/useMutateFavoritePost';
 
 type FeedDetailScreenProps = CompositeScreenProps<
   StackScreenProps<FeedStackParamList, typeof feedNavigations.FEED_DETAIL>,
@@ -47,6 +48,7 @@ function FeedDetailScreen({route, navigation}: FeedDetailScreenProps) {
   const {setMoveLocation} = useLocationStore();
   const detailOption = useModal();
   const {setDetailPost} = useDetailStore();
+  const favoriteMutation = useMutateFavoritePost();
 
   useEffect(() => {
     post && setDetailPost(post);
@@ -62,6 +64,10 @@ function FeedDetailScreen({route, navigation}: FeedDetailScreenProps) {
     navigation.navigate(mainNavigations.HOME, {
       screen: mapNavigations.MAP_HOME,
     });
+  };
+
+  const handlePressFavorite = () => {
+    favoriteMutation.mutate(post.id);
   };
 
   return (
@@ -167,8 +173,13 @@ function FeedDetailScreen({route, navigation}: FeedDetailScreenProps) {
             style={({pressed}) => [
               pressed && styles.bookmarkPressedContainer,
               styles.bookmarkContainer,
-            ]}>
-            <Octicons name="star-fill" size={30} color={colors.GRAY_100} />
+            ]}
+            onPress={handlePressFavorite}>
+            <Octicons
+              name="star-fill"
+              size={30}
+              color={post.isFavorite ? colors.YELLOW_500 : colors.GRAY_100}
+            />
           </Pressable>
           <CustomButton
             label="위치보기"
