8adee092f14b8b3bda7d32b0ef873b33a95bd02d
강주현 <juhyunk0820@naver.com>
2025-04-02 22:32:37 +0900

    캘린더 컴포넌트 입력 인자 추가 수정

--- Matzip_FE/src/screens/calendar/CalendarHomeScreen.tsx
+++ Matzip_FE/src/screens/calendar/CalendarHomeScreen.tsx
@@ -1,24 +1,39 @@
 import Calendar from '@/components/calendar/Calendar';
+import {colors} from '@/constants';
 import {getMonthYearDetails, getNewMonthYear} from '@/utils';
 import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 function CalendarHomeScreen() {
   const currentMonthYear = getMonthYearDetails(new Date());
   const [monthYear, setMonthYear] = useState(currentMonthYear);
+  const [selectedDate, setSelectedDate] = useState(0);
+
+  const handlePressDate = (date: number) => {
+    setSelectedDate(date);
+  };
 
   const handleUpdateMonth = (increment: number) => {
     setMonthYear(prev => getNewMonthYear(prev, increment));
   };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Calendar monthYear={monthYear} onChangeMonth={handleUpdateMonth} />
+      <Calendar
+        monthYear={monthYear}
+        onChangeMonth={handleUpdateMonth}
+        selectedDate={selectedDate}
+        onPressDate={handlePressDate}
+      />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {},
-});
-
 export default CalendarHomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+});
