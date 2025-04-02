e91c8408caf585983861f80e72a79ac41ca0a573
강주현 <juhyunk0820@naver.com>
2025-04-02 21:00:44 +0900

    캘린더 홈 화면 수정: 캘린더 컴포넌트 반영

--- Matzip_FE/src/screens/calendar/CalendarHomeScreen.tsx
+++ Matzip_FE/src/screens/calendar/CalendarHomeScreen.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import Calendar from '@/components/calendar/Calendar';
+import {getMonthYearDetails, getNewMonthYear} from '@/utils';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 function CalendarHomeScreen() {
+  const currentMonthYear = getMonthYearDetails(new Date());
+  const [monthYear, setMonthYear] = useState(currentMonthYear);
+
+  const handleUpdateMonth = (increment: number) => {
+    setMonthYear(prev => getNewMonthYear(prev, increment));
+  };
   return (
-    <View>
-      <Text>캘린더</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Calendar monthYear={monthYear} onChangeMonth={handleUpdateMonth} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
 
 export default CalendarHomeScreen;
