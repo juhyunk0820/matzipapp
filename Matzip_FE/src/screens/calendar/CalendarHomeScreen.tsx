1b1c4ede6a0ced2f69026b658313eaeac1b7443c
강주현 <juhyunk0820@naver.com>
2025-04-02 23:29:08 +0900

    EventList 컴포넌트 추가

--- Matzip_FE/src/screens/calendar/CalendarHomeScreen.tsx
+++ Matzip_FE/src/screens/calendar/CalendarHomeScreen.tsx
@@ -1,5 +1,7 @@
 import Calendar from '@/components/calendar/Calendar';
+import EventList from '@/components/calendar/EventList';
 import {colors} from '@/constants';
+import useGetCalendarPost from '@/hooks/queries/useGetCalendarPost';
 import {getMonthYearDetails, getNewMonthYear} from '@/utils';
 import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
@@ -8,6 +10,15 @@ function CalendarHomeScreen() {
   const currentMonthYear = getMonthYearDetails(new Date());
   const [monthYear, setMonthYear] = useState(currentMonthYear);
   const [selectedDate, setSelectedDate] = useState(0);
+  const {
+    data: posts,
+    isPending,
+    isError,
+  } = useGetCalendarPost(monthYear.year, monthYear.month);
+
+  if (isPending || isError) {
+    return <></>;
+  }
 
   const handlePressDate = (date: number) => {
     setSelectedDate(date);
@@ -21,10 +32,12 @@ function CalendarHomeScreen() {
     <SafeAreaView style={styles.container}>
       <Calendar
         monthYear={monthYear}
+        schedules={posts}
         onChangeMonth={handleUpdateMonth}
         selectedDate={selectedDate}
         onPressDate={handlePressDate}
       />
+      <EventList posts={posts[selectedDate]} />
     </SafeAreaView>
   );
 }
