376dc38e526d3bad53e23e0868cac3d1589ab8a1
강주현 <juhyunk0820@naver.com>
2025-04-02 20:59:59 +0900

    Calendar컴포넌트: 헤드라인 + day컴포넌트 종합

---
+++ Matzip_FE/src/components/calendar/Calendar.tsx
@@ -0,0 +1,65 @@
+import {colors} from '@/constants';
+import React from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import DayOfWeeks from './DayOfWeeks';
+import {MonthYear} from '@/utils';
+
+interface CanlendarProps {
+  monthYear: MonthYear;
+  onChangeMonth: (increment: number) => void;
+}
+
+function Calendar({monthYear, onChangeMonth}: CanlendarProps) {
+  const {month, year} = monthYear;
+
+  return (
+    <>
+      <View style={styles.headerContainer}>
+        <Pressable
+          onPress={() => onChangeMonth(-1)}
+          style={styles.monthButtonContainer}>
+          <Ionicons name="arrow-back" size={25} color={colors.BLACK} />
+        </Pressable>
+        <Pressable style={styles.monthYearContainer}>
+          <Text style={styles.titleText}>
+            {year}년 {month}월
+          </Text>
+          <MaterialIcons
+            name="keyboard-arrow-down"
+            size={20}
+            color={colors.GRAY_500}
+          />
+        </Pressable>
+        <Pressable style={styles.monthButtonContainer}>
+          <Ionicons name="arrow-forward" size={25} color={colors.BLACK} />
+        </Pressable>
+      </View>
+      <DayOfWeeks />
+    </>
+  );
+}
+
+export default Calendar;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 25,
+    marginVertical: 16,
+  },
+  monthButtonContainer: {
+    padding: 10,
+  },
+  monthYearContainer: {
+    flexDirection: 'row',
+  },
+  titleText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: colors.BLACK,
+  },
+});
