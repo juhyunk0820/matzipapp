568f57fc01fe712327ab7e2de386178b7792ee5e
강주현 <juhyunk0820@naver.com>
2025-04-07 23:01:21 +0900

    SearchLocationScreen: 기능 구현중

---
+++ Matzip_FE/src/screens/map/SearchLocationScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+interface SearchLocationScreenProps {}
+
+function SearchLocationScreen() {
+  return <View style={styles.container}></View>;
+}
+
+export default SearchLocationScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
