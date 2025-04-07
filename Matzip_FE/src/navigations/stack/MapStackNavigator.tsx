c28a0a0454dbadbeb8f493975670627a2eb6a050
강주현 <juhyunk0820@naver.com>
2025-04-07 22:59:32 +0900

    MapStacknavigator 수정: SearchLocationScreen 추가

--- Matzip_FE/src/navigations/stack/MapStackNavigator.tsx
+++ Matzip_FE/src/navigations/stack/MapStackNavigator.tsx
@@ -5,10 +5,12 @@ import MapHomeScreen from '@/screens/map/MapHomeScreen';
 import {mapNavigations} from '@/constants';
 import AddPostScreen from '@/screens/map/AddPostScreen';
 import {LatLng} from 'react-native-maps';
+import SearchLocationScreen from '@/screens/map/SearchLocationScreen';
 
 export type MapStackParamList = {
   [mapNavigations.MAP_HOME]: undefined;
   [mapNavigations.ADD_POST]: {location: LatLng};
+  [mapNavigations.SEARCH_LOCATION]: undefined;
 };
 
 const Stack = createStackNavigator<MapStackParamList>();
@@ -43,6 +45,14 @@ function MapStackNavigator() {
         options={{
           headerTitle: '장소 추가',
           headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name={mapNavigations.SEARCH_LOCATION}
+        component={SearchLocationScreen}
+        options={{
+          presentation: 'modal',
+          headerTitle: '장소 검색',
         }}
       />
     </Stack.Navigator>
