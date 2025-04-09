b0eedaed8907c3f1b34fcb07c2201d2aba86171e
강주현 <juhyunk0820@naver.com>
2025-04-09 19:21:50 +0900

    AppleButton 생성 및 애플 로그인 기능 구현

--- Matzip_FE/src/screens/Auth/AuthHomeScreen.tsx
+++ Matzip_FE/src/screens/Auth/AuthHomeScreen.tsx
@@ -3,6 +3,7 @@ import {StackScreenProps} from '@react-navigation/stack';
 import {
   Dimensions,
   Image,
+  Platform,
   Pressable,
   SafeAreaView,
   StyleSheet,
@@ -10,10 +11,15 @@ import {
   View,
 } from 'react-native';
 
+import appleAuth, {
+  AppleButton,
+} from '@invertase/react-native-apple-authentication';
 import {AuthStackParamList} from '@/navigations/stack/AuthStackNavigator';
 import CustomButton from '@/components/common/CustomButton';
 import {authNavigations, colors} from '@/constants';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import useAuth from '@/hooks/queries/useAuth';
+import Toast from 'react-native-toast-message';
 
 type AuthHomeScreenProps = StackScreenProps<
   AuthStackParamList,
@@ -21,6 +27,32 @@ type AuthHomeScreenProps = StackScreenProps<
 >;
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
+  const {appleLoginMutation} = useAuth();
+  const handlePressAppleLogin = async () => {
+    try {
+      const {identityToken, fullName} = await appleAuth.performRequest({
+        requestedOperation: appleAuth.Operation.LOGIN,
+        requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+      });
+
+      if (identityToken) {
+        appleLoginMutation.mutate({
+          identityToken,
+          appId: 'org.reactjs.native.example.Matzip-FE',
+          nickname: fullName?.givenName || '',
+        });
+      }
+    } catch (error: any) {
+      if (error.code !== appleAuth.Error.CANCELED) {
+        Toast.show({
+          type: 'error',
+          text1: '애플 로그인에 실패했습니다.',
+          text2: '다시 시도해주세요.',
+        });
+      }
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.imageContainer}>
@@ -30,7 +62,20 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
           source={require('@/assets/MATZIP.png')}
         />
       </View>
+
       <View style={styles.buttonContainer}>
+        {Platform.OS === 'ios' && (
+          <AppleButton
+            buttonStyle={AppleButton.Style.BLACK}
+            buttonType={AppleButton.Type.SIGN_IN}
+            style={{
+              width: Dimensions.get('screen').width - 60,
+              height: 55,
+            }}
+            cornerRadius={3}
+            onPress={handlePressAppleLogin}
+          />
+        )}
         <CustomButton
           label="카카오 로그인하기"
           onPress={() => navigation.navigate(authNavigations.KAKAO)}
