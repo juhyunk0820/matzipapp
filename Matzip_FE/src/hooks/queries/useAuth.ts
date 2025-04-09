76390374e7ddb2690c5c4871272e0fb77b0e21d7
강주현 <juhyunk0820@naver.com>
2025-04-09 19:21:28 +0900

    apple Login useMutation 생성

--- Matzip_FE/src/hooks/queries/useAuth.ts
+++ Matzip_FE/src/hooks/queries/useAuth.ts
@@ -4,6 +4,7 @@ import {MutationFunction, useMutation, useQuery} from '@tanstack/react-query';
 import {
   ResponseProfile,
   ResponseToken,
+  appleLogin,
   getAccessToken,
   getProfile,
   kakaoLogin,
@@ -59,6 +60,10 @@ function useEmailLogin(mutationOptions?: UseMutationCustomOptions) {
 
 function useKakaoLogin(mutationOptions?: UseMutationCustomOptions) {
   return useLogin(kakaoLogin, mutationOptions);
+}
+
+function useAppleLogin(mutationOptions?: UseMutationCustomOptions) {
+  return useLogin(appleLogin, mutationOptions);
 }
 
 function useGetRefreshToken() {
@@ -119,6 +124,7 @@ function useAuth() {
   const isLogin = getProfileQuery.isSuccess;
   const emailLoginMutation = useEmailLogin();
   const kakaoLoginMutation = useKakaoLogin();
+  const appleLoginMutation = useAppleLogin();
 
   const logoutMutation = useLogout();
 
@@ -126,6 +132,7 @@ function useAuth() {
     signupMutation,
     emailLoginMutation,
     kakaoLoginMutation,
+    appleLoginMutation,
     getProfileQuery,
     isLogin,
     logoutMutation,
