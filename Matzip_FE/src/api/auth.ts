aca37de5814c323bd4349a2d6a03a156ec2d0bcb
강주현 <juhyunk0820@naver.com>
2025-04-09 19:21:05 +0900

    auth api 추가: apple

--- Matzip_FE/src/api/auth.ts
+++ Matzip_FE/src/api/auth.ts
@@ -32,6 +32,19 @@ const kakaoLogin = async (token: string): Promise<ResponseToken> => {
   return data;
 };
 
+type RequestAppleIdentity = {
+  identityToken: string;
+  appId: string;
+  nickname: string;
+};
+
+const appleLogin = async (
+  body: RequestAppleIdentity,
+): Promise<ResponseToken> => {
+  const {data} = await axiosInstance.post('auth/oauth/apple', body);
+  return data;
+};
+
 type ResponseProfile = Profile & Category;
 
 const getProfile = async (): Promise<ResponseProfile> => {
@@ -56,5 +69,13 @@ const logout = async () => {
   await axiosInstance.post('/auth/logout');
 };
 
-export {postSignup, postLogin, getProfile, getAccessToken, logout, kakaoLogin};
+export {
+  postSignup,
+  postLogin,
+  getProfile,
+  getAccessToken,
+  logout,
+  kakaoLogin,
+  appleLogin,
+};
 export type {RequestUser, ResponseToken, ResponseProfile};
