eb3601c4851ecea0ad6517be6d065a1c0cca93e9
강주현 <juhyunk0820@naver.com>
2025-04-10 19:16:49 +0900

    카테고리 수정 버튼 클릭: 카테고리 수정화면 이동

--- Matzip_FE/src/screens/setting/SettingHomeScreen.tsx
+++ Matzip_FE/src/screens/setting/SettingHomeScreen.tsx
@@ -22,11 +22,19 @@ function SettingHomeScreen({navigation}: SettingHomeScreenProps) {
   const handlePressEditProfile = () => {
     navigation.navigate(settingNavigations.EDIT_PROFILE);
   };
+
+  const handlePressEditCategory = () => {
+    navigation.navigate(settingNavigations.EDIT_CATEGORY);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.space} />
       <SettingItem title="프로필 수정" onPress={handlePressEditProfile} />
-      <SettingItem title="마커 카테고리 수정" />
+      <SettingItem
+        title="마커 카테고리 수정"
+        onPress={handlePressEditCategory}
+      />
       <View style={styles.space} />
       <SettingItem
         title="로그아웃"
