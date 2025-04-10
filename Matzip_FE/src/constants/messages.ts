1a30fd2716a5dc2618bd2948d1c7b02a1c5e42af
강주현 <juhyunk0820@naver.com>
2025-04-10 19:13:24 +0900

    alerts 추가: DELETE_ACCOUNT message

--- Matzip_FE/src/constants/messages.ts
+++ Matzip_FE/src/constants/messages.ts
@@ -15,6 +15,10 @@ const alerts = {
     TITLE: '삭제하시겠습니까?',
     DESCRIPTION: '피드와 지도에서 모두 삭제됩니다.',
   },
+  DELETE_ACCOUNT: {
+    TITLE: '정말 탈퇴하시겠습니까?',
+    DESCRIPTION: '회원정보가 삭제되며 복구할 수 없습니다.',
+  },
 } as const;
 
 const errorMessages = {
