a0824131d664cd580f2b7548d85ac6bb28b2124d
강주현 <juhyunk0820@naver.com>
2025-04-10 19:16:08 +0900

    EditProfileScreen 기능 추가: 회원탈퇴 클릭시 회원탈퇴 화면 이동

--- Matzip_FE/src/screens/setting/EditProfileScreen.tsx
+++ Matzip_FE/src/screens/setting/EditProfileScreen.tsx
@@ -14,7 +14,7 @@ import {
 import InputField from '@/components/common/InputField';
 import useImagePicker from '@/hooks/useImagePicker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {colors, errorMessages} from '@/constants';
+import {colors, errorMessages, settingNavigations} from '@/constants';
 import useModal from '@/hooks/useModal';
 import EditProfileImageOption from '@/components/setting/EditProfileImageOption';
 import {StackScreenProps} from '@react-navigation/stack';
@@ -117,7 +117,9 @@ function EditProfileScreen({navigation}: EditProfileScreenProps) {
         touched={editProfile.touched.nickname}
         placeholder="닉네임을 입력해주세요."
       />
-      <Pressable style={styles.deleteAccountContainer}>
+      <Pressable
+        style={styles.deleteAccountContainer}
+        onPress={() => navigation.navigate(settingNavigations.DELETE_ACCOUNT)}>
         <Ionicons name="remove-circle-sharp" size={20} color={colors.RED_500} />
         <Text style={styles.deleteAccountText}>회원탈퇴</Text>
       </Pressable>
