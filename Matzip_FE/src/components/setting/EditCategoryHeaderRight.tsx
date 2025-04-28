40263ea7794370ff590e59a31f0f0a6444de4f00
강주현 <juhyunk0820@naver.com>
2025-04-28 19:30:21 +0900

    카테고리 수정 컴포넌트 헤더 버튼 구현

---
+++ Matzip_FE/src/components/setting/EditCategoryHeaderRight.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HeaderButton from '../common/HeaderButton';
+
+function EditCategoryHeaderRight(onSubmit: () => void) {
+  return <HeaderButton labelText="저장" />;
+}
+
+export default EditCategoryHeaderRight;
