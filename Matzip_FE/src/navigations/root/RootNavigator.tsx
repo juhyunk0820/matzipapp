f031e5f8078ef3e2815baf5fcac4023da5a54f4b
강주현 <juhyunk0820@naver.com>
2025-04-30 18:27:49 +0900

    RootNavigator - SplashScreen 동안 로그인화면 감춤 설정

--- Matzip_FE/src/navigations/root/RootNavigator.tsx
+++ Matzip_FE/src/navigations/root/RootNavigator.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MainDrawerNavigator from '../drawer/MainDrawerNavigator';
 import AuthStackNavigator from '../stack/AuthStackNavigator';
 import useAuth from '@/hooks/queries/useAuth';
+import SplashScreen from 'react-native-splash-screen';
 
 function RootNavigator() {
-  const {isLogin} = useAuth();
+  const {isLogin, isLoginLoading} = useAuth();
+
+  useEffect(() => {
+    if (!isLoginLoading) {
+      SplashScreen.hide();
+    }
+  }, [isLoginLoading]);
 
   return <>{isLogin ? <MainDrawerNavigator /> : <AuthStackNavigator />}</>;
 }
