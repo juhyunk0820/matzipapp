36ee256a7b1a0d476f5cbbad39fe289ae0af5f1b
강주현 <juhyunk0820@naver.com>
2025-04-30 18:27:25 +0900

    refreshToken - isPending 추가

--- Matzip_FE/src/hooks/queries/useAuth.ts
+++ Matzip_FE/src/hooks/queries/useAuth.ts
@@ -71,7 +71,7 @@ function useAppleLogin(mutationOptions?: UseMutationCustomOptions) {
 }
 
 function useGetRefreshToken() {
-  const {data, error, isSuccess, isError} = useQuery({
+  const {data, error, isSuccess, isError, isPending} = useQuery({
     queryKey: [queryKeys.AUTH, queryKeys.GET_ACCESS_TOKEN],
     queryFn: getAccessToken,
     staleTime: numbers.ACCESS_TOKEN_REFRESH_TIME,
@@ -94,7 +94,7 @@ function useGetRefreshToken() {
     }
   }, [isError]);
 
-  return {isSuccess, isError};
+  return {isSuccess, isError, isPending};
 }
 
 type ResponseSelectProfile = {categories: Category} & Profile;
@@ -187,6 +187,7 @@ function useAuth() {
     onSuccess: () => logoutMutation.mutate(null),
   });
   const categoryMutation = useMutateCategory();
+  const isLoginLoading = refreshTokenQuery.isPending;
 
   return {
     signupMutation,
@@ -199,6 +200,7 @@ function useAuth() {
     updateProfileMutation,
     deleteAccountMutation,
     categoryMutation,
+    isLoginLoading,
   };
 }
 
