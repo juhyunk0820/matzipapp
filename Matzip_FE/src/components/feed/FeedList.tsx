7cde038a23245a375d94adcc90b62dced97925c0
강주현 <juhyunk0820@naver.com>
2025-05-02 18:47:16 +0900

    FeedList 테마 색상 적용 및 오류 사항 수정

--- Matzip_FE/src/components/feed/FeedList.tsx
+++ Matzip_FE/src/components/feed/FeedList.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import useGetInfinitePosts from '@/hooks/queries/useGetInfinitePosts';
 import FeedItem from './FeedItem';
+import useThemeStore from '@/store/useThemeStore';
 
 function FeedList() {
+  const {theme} = useThemeStore();
   const {
     data: posts,
     fetchNextPage,
@@ -31,13 +33,18 @@ function FeedList() {
       renderItem={({item}) => <FeedItem post={item} />}
       keyExtractor={item => String(item.id)}
       numColumns={2}
+      ListEmptyComponent={
+        <View>
+          <Text style={{textAlign: 'center'}}>즐겨찾기한 장소가 없습니다.</Text>
+        </View>
+      }
       contentContainerStyle={styles.contentContainer}
       onEndReached={handleEndReached}
       onEndReachedThreshold={0.5}
       refreshing={isRefreshing}
       onRefresh={handleRefresh}
       scrollIndicatorInsets={{right: 1}}
-      indicatorStyle="black"
+      indicatorStyle={theme === 'dark' ? 'white' : 'black'}
     />
   );
 }
