8a84bd36c083c3342c8de5428ac28ce849bbcc89
강주현 <juhyunk0820@naver.com>
2025-05-07 01:57:01 +0900

    범례CompoundOption 컴포넌트

--- Matzip_FE/src/components/setting/MapLegendOption.tsx
+++ Matzip_FE/src/components/setting/MapLegendOption.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
 import {CompoundOption} from '../common/CompoundOption';
+import useLegendStorage from '@/hooks/useLegendStorage';
 
 interface MapLegendOptionProps {
   isVisible: boolean;
@@ -8,7 +8,43 @@ interface MapLegendOptionProps {
 }
 
 function MapLegendOption({isVisible, hideOption}: MapLegendOptionProps) {
-  return <CompoundOption></CompoundOption>;
+  const {isVisible: isVisibleLegend, set} = useLegendStorage();
+
+  const handlePressShow = () => {
+    set(true);
+    hideOption();
+  };
+
+  const handlePressHide = () => {
+    set(false);
+    hideOption();
+  };
+
+  return (
+    <CompoundOption isVisible={isVisible} hideOption={hideOption}>
+      <CompoundOption.Background>
+        <CompoundOption.Container>
+          <CompoundOption.Button
+            onPress={handlePressShow}
+            isChecked={isVisibleLegend}>
+            표시하기
+          </CompoundOption.Button>
+          <CompoundOption.Divider />
+          <CompoundOption.Button
+            onPress={handlePressHide}
+            isChecked={!isVisibleLegend}>
+            숨기기
+          </CompoundOption.Button>
+        </CompoundOption.Container>
+
+        <CompoundOption.Container>
+          <CompoundOption.Button onPress={hideOption}>
+            취소
+          </CompoundOption.Button>
+        </CompoundOption.Container>
+      </CompoundOption.Background>
+    </CompoundOption>
+  );
 }
 
 export default MapLegendOption;
